7455d16fc499b810e2fe05588ec2ab0d2f271614
Sebastian Mejia G <sebasmejia084@gmail.com>
2024-05-20 16:52:06 -0500

    ViewModel refactor and unit testing

--- View/MovieList/MovieListViewController.swift
+++ View/MovieList/MovieListViewController.swift
@@ -18,147 +18,102 @@ class MovieListViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     let cellIdentifier = "MovieTableViewCell"
-    var viewModel = MovieViewModel()
-    var moviesList: [Movie] = []
-    var filteredMoviesList: [Movie] = []
-    var isAdultSelected: Bool = UserDefaults.standard.bool(forKey: UserDefaultsKeys.isAdultSelectedKey)
-    var origianlLanguage: String =  UserDefaults.standard.string(forKey: UserDefaultsKeys.selectedLanguageKey) ?? "en"
-    var voteAverage = ( 
-        UserDefaults.standard.string(forKey: UserDefaultsKeys.minVoteAverageKey) ?? "0",
-        UserDefaults.standard.string(forKey: UserDefaultsKeys.maxVoteAverageKey) ?? "10")
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        viewModel.delegate = self
-        setupView()
-        activityIndicator.startAnimating()
-        applyFilters(includeAdult: isAdultSelected, language: origianlLanguage, voteAvg: voteAverage)
-        emptyResultLabel.text = "There are no results with the selected filters"
-    }
-    
-    @objc func segmentChanged() {
-        applyFilters(includeAdult: isAdultSelected, language: origianlLanguage, voteAvg: voteAverage)
-    }
-    
-    func applyFilters(includeAdult: Bool, language: String, voteAvg: (String, String)) {
-        updateLocalVars()
-        moviesDataTableView.isHidden = true
-        viewModel.getMovies(isPopular: generalFilterSegmentedControl.selectedSegmentIndex == 0 ? true : false, includeAdult: includeAdult, language: language)
-        getFilteredData()
-        self.moviesDataTableView.isHidden = !self.filteredMoviesList.isEmpty
+      var viewModel = MovieViewModel()
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          viewModel.delegate = self
+          setupView()
+          activityIndicator.startAnimating()
+          viewModel.getMovies(isPopular: true, includeAdult: viewModel.isAdultSelected, language: viewModel.originalLanguage)
+          emptyResultLabel.text = "There are no results with the selected filters"
+      }
+      
+      @objc func segmentChanged() {
+          viewModel.getMovies(isPopular: generalFilterSegmentedControl.selectedSegmentIndex == 0, includeAdult: viewModel.isAdultSelected, language: viewModel.originalLanguage)
+      }
+      
+      func setupView() {
+          searchBar.delegate = self
+          setupTableViews()
+          setupFilterButton()
+          generalFilterSegmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
+      }
+      
+      func setupTableViews() {
+          moviesDataTableView.dataSource = self
+          moviesDataTableView.delegate = self
+          moviesDataTableView.register(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
+      }
+      
+      func setupFilterButton() {
+          filterButton.addTarget(self, action: #selector(showFilterView), for: .touchUpInside)
+      }
+      
+      @objc func showFilterView() {
+          let storyboard = UIStoryboard(name: "FiltersViewStoryboard", bundle: nil)
+          if let filterVC = storyboard.instantiateViewController(withIdentifier: "FiltersViewController") as? FiltersViewController {
+              filterVC.delegate = self
+              present(filterVC, animated: true, completion: nil)
+          }
+      }
+  }
 
-    }
-    
-    func setupView() {
-        searchBar.delegate = self
-        setupTableViews()
-        setupFilterButton()
-        generalFilterSegmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
-    }
-    
-    func setupTableViews() {
-        moviesDataTableView.dataSource = self
-        moviesDataTableView.delegate = self
-        moviesDataTableView.register(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
-        
-    }
-    
-    func setupFilterButton() {
-        filterButton.addTarget(self, action: #selector(showFilterView), for: .touchUpInside)
-    }
-    
-    @objc func showFilterView() {
-        let storyboard = UIStoryboard(name: "FiltersViewStoryboard", bundle: nil)
-        if let filterVC = storyboard.instantiateViewController(withIdentifier: "FiltersViewController") as? FiltersViewController {
-            filterVC.delegate = self
-            present(filterVC, animated: true, completion: nil)
-        }
-    }
-    
-    func getFilteredData(searchedText: String = "") {
-        var minVoteAverage = Double(voteAverage.0) ?? 0.0
-        var maxVoteAverage = Double(voteAverage.1) ?? 10.0
-        
-        if minVoteAverage > maxVoteAverage {
-                minVoteAverage = maxVoteAverage
-            }
-        
-        filteredMoviesList = moviesList.filter { movie in
-            let matchesTitle = searchedText.isEmpty ? true : movie.title.lowercased().contains(searchedText.lowercased())
-            let matchesVoteAverage = (minVoteAverage...maxVoteAverage).contains(Double(movie.voteAverage))
-            return matchesTitle && matchesVoteAverage
-        }.filter { !$0.overview.isEmpty }
-        
-        DispatchQueue.main.async {
-            self.moviesDataTableView.reloadData()
-            self.moviesDataTableView.isHidden = !self.filteredMoviesList.isEmpty
-        }
-    }
-    func updateLocalVars() {
-         isAdultSelected = UserDefaults.standard.bool(forKey: UserDefaultsKeys.isAdultSelectedKey)
-         origianlLanguage =  UserDefaults.standard.string(forKey: UserDefaultsKeys.selectedLanguageKey) ?? "en"
-         voteAverage = (
-            UserDefaults.standard.string(forKey: UserDefaultsKeys.minVoteAverageKey) ?? "0",
-            UserDefaults.standard.string(forKey: UserDefaultsKeys.maxVoteAverageKey) ?? "10")
-    }
+  extension MovieListViewController: MovieViewModelDelegate {
+      
+      func didReceiveMovies(_ movies: [Movie]) {
+          DispatchQueue.main.async {
+              self.moviesDataTableView.reloadData()
+              self.moviesDataTableView.isHidden = movies.isEmpty
+              self.activityIndicator.stopAnimating()
+          }
+      }
+      
+      func didFailWithError(_ error: Error) {
+          DispatchQueue.main.async {
+              print("Failed to get movies: \(error.localizedDescription)")
+              self.activityIndicator.stopAnimating()
+          }
+      }
+  }
 
-    
-}
+  extension MovieListViewController: UITableViewDataSource, UITableViewDelegate {
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return viewModel.filteredMoviesList.count
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MovieTableViewCell else {
+              return UITableViewCell()
+          }
+          if !viewModel.filteredMoviesList.isEmpty {
+              let movie = viewModel.filteredMoviesList[indexPath.row]
+              cell.config(movie: movie)
+          }
+          return cell
+      }
+  }
 
-extension MovieListViewController: MovieViewModelDelegate {
-    
-    func didReceiveMovies(_ movies: [Movie]) {
-        moviesList = movies
-        getFilteredData()
-        DispatchQueue.main.async {
-            self.moviesDataTableView.reloadData()
-            self.moviesDataTableView.isHidden = false
-        }
-    }
-    
-    func didFailWithError(_ error: Error) {
-        print("Failed to get movies: \(error.localizedDescription)")
-    }
-}
+  extension MovieListViewController: FilterViewControllerDelegate {
+      
+      func didApplyFilters(includeAdult: Bool, language: String, voteAvg: (String, String)) {
+          viewModel.updateFilters(includeAdult: includeAdult, language: language, voteAvg: voteAvg)
+      }
+  }
 
-extension MovieListViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredMoviesList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MovieTableViewCell else {
-            return UITableViewCell()
-        }
-        if !filteredMoviesList.isEmpty {
-            let movie = filteredMoviesList[indexPath.row]
-            cell.config(movie: movie)
-        }
-        return cell
-    }
-}
-
-
-extension MovieListViewController: FilterViewControllerDelegate {
-    
-    func didApplyFilters(includeAdult: Bool, language: String, voteAvg: (String, String)) {
-        applyFilters(includeAdult: includeAdult, language: language, voteAvg: voteAvg)
-    }
-}
-
-extension MovieListViewController: UISearchBarDelegate {
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        getFilteredData(searchedText: searchBar.text ?? String())
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.endEditing(true)
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.endEditing(true)
-        searchBar.text = String()
-        getFilteredData()
-    }
-}
+  extension MovieListViewController: UISearchBarDelegate {
+      
+      func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+          viewModel.filterMovies(searchedText: searchText)
+      }
+      
+      func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+          searchBar.endEditing(true)
+      }
+      
+      func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+          searchBar.endEditing(true)
+          searchBar.text = ""
+          viewModel.filterMovies()
+      }
+  }
